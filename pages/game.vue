6cbe1b836de9cf2699a1e6fb5ee9dff583d90c9f
Loshido <loshido@pm.me>
2023-02-26 17:50:35 +0100

    Spinner upgraded.

--- pages/game.vue
+++ pages/game.vue
@@ -36,9 +36,7 @@
             </audio>
         </form>
     </main>
-    <svg v-else class="spinner" viewBox="0 0 50 50">
-        <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
-    </svg>
+    <div v-else class="spinner"></div>
 </template>
 
 <script>
--- pages/leaderboard.vue
+++ pages/leaderboard.vue
@@ -22,9 +22,7 @@
                 </tr>
             </tbody>
         </table>
-        <svg v-else class="spinner" viewBox="0 0 50 50">
-            <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
-        </svg>
+        <div v-else class="spinner"></div>
     </main>
 </template>
 
